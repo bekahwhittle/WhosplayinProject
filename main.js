fdade93eed84dbafbc18c42e97e661581d718e41
rdw1995 <whittlebekah@gmail.com>
2017-02-02 10:43:57 -0600

    js practice

---
+++ main.js
@@ -0,0 +1,15 @@
+var getReview = function (movie) {
+    switch (movie) {
+      case "Toy Story 2":
+        console.log("Great story. Mean prospector.");
+        break;
+      case "Finding Nemo":
+        console.log("Cool animation, and funny turtles.");
+        break;
+      case "The Lion King":
+        console.log("Great Songs");
+        break;
+      default:
+        console.log("I don't know");
+    }
+};
